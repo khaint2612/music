0f9b9480802d03870ae26303d7135b16721b3a51
khaint2612 <75000790+khaint2612@users.noreply.github.com>
2022-03-01 08:21:28 +0700

    mp3
    mp3

---
+++ mp3/Song.java
@@ -0,0 +1,30 @@
+package mp3;
+
+public class Song{
+    String title;
+    double duration;
+
+    public Song(){
+
+    }
+    
+    public Song(String title,double duration){
+        this.title = title;
+        this.duration =duration;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public double getDuration(){
+        return duration;
+    }
+    @Override
+    public String toString(){
+        return "Song{"+
+                "title='"+title+'\''+
+                ", duration="+duration+
+                '}';
+    }
+}
